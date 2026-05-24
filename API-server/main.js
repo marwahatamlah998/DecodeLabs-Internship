d8a815ec9cc1b98369e3e9a2ff837df59e426a40
MarwahHatamleh <marwahatamleh98@gmail.com>
2026-05-24 18:47:02 +0300

    HTTPS request with ex

---
+++ API-server/main.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+app.use(express.json());
+
+let users = [];
+
+app.get("/api/users/", (req, res) => {
+  res.json(users);
+});
+
+app.post("/api/users/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  console.log("Received data:", users);
+
+  res.json({
+    message: "Data received successfully",
+    users: users,
+  });
+});
+
+app.put("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  // console.log(Number(req.params.id))
+  const updatedData = req.body;
+  users = users.map((user) =>
+    Number(user.id) === id ? { ...user, ...updatedData } : user,
+  );
+  res.json(users);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const oldLength = users.length;
+
+  users = users.filter((user) => Number(user.id) !== id);
+
+  if (users.length === oldLength) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.json({
+    message: "User deleted successfully",
+    users,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
